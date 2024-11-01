031ff77beef8d1f807809fb2d745f02327dc529e
leoshen <shen.leo@outlook.com>
2024-10-31 22:42:34 -0700

    fix: update geolocation functions for edge cases

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -791,6 +791,7 @@ describe("InsightFacade", function () {
 			"[valid/queryWithGeolocationFieldsInGROUPAndAPPLY.json] Query with Geolocation Fields in GROUP and APPLY",
 			checkQuery
 		);
+		it("[valid/queryForNon-ExistentRoomAddress.json] Query for Non-Existent Room Address", checkQuery);
 
 		it(
 			"[invalid/query_invalid_latitude_longitude_field.json] Invalid Latitude/Longitude Query with Incorrect Field Name",
